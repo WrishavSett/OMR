a229168f8a2645b372c34470bb6e8fd0e573a958
Rohit <rohit@gmail.com>
2024-05-20 15:22:23 +0530

    Update:- 1. Roll Number Update is working 2. Generating csv

--- scanomr.py
+++ scanomr.py
@@ -10,6 +10,9 @@ import pandas as pd
 
 
 OPTIONS_MAP = {0:"a",1:"b",2:"c",3:"d",4:"Not Selected"}
+OPTIONS_MAP_ROLL = {0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"-NS/MS-"}
+
+
 peaks_top = 0
 peaks_bottom = 0
 calculatedanchorx = 0
@@ -60,37 +63,67 @@ def get_image_sections(image,coords):
   d = image[coords["d"][0]:coords["d"][1],coords["d"][2]:coords["d"][3]]
   return region,[a,b,c,d]
 
+def get_image_sectionsv2(image,coords):
+  optionimages = []
+  for key in coords.keys():
+    if key == "region":
+      region = image[coords[key][0]:coords[key][1],coords[key][2]:coords[key][3]]
+    else:
+      tmpoptionimage = image[coords[key][0]:coords[key][1],coords[key][2]:coords[key][3]]
+      optionimages.append(tmpoptionimage)
+  return region,optionimages
+
 def get_result(region,options,show_region=False):
-  global epsilon
   result = []
+  countselected = 0
   for i in options:
-    result.append(np.mean(i))
+    result.append(np.mean(255-i))
   result_arr = np.array(result)
-  diffthresh = np.sort(result_arr)[1] - np.sort(result_arr)[0]
+  for result_arr_element in result_arr:
+    if (result_arr_element > 80):
+      countselected += 1
+  # print(result_arr)
   if show_region:
     plt.imshow(region)
-    plt.show() 
-  option_index = np.argmin(result_arr)
-  if diffthresh < epsilon:
+  option_index = np.argmax(result_arr)
+  if countselected > 1:
     option_index = 4
   return OPTIONS_MAP[option_index]
+
+def get_roll(region,options,show_region=False):
+  result = []
+  countselected = 0
+  for i in options:
+    result.append(np.mean(255-i))
+  result_arr = np.array(result)
+  # print(result_arr)
+  for result_arr_element in result_arr:
+    if (result_arr_element > 100):
+      countselected += 1
+  if show_region:
+    plt.imshow(region)
+    plt.show()
+  option_index = np.argmax(result_arr)
+  print(option_index)
+  if countselected > 1:
+    option_index = 10
+  return OPTIONS_MAP_ROLL[option_index]
+
+def createmetadatfile(anchornumber,data):
+  metadatalist = []
+  for data_element in data:
+    if data_element["name"] != "anchor":
+      block_metadata = getmetadataforblock(data[anchornumber],data_element)
+      metadatalist.append(block_metadata)
+  with open("metadata.json", "w") as outfile:
+    json.dump(metadatalist, outfile)
+
+    
 
 def showsectionandgetregion(image,data,calculatedanchorx,calculatedanchory,sortedanchor,anchornumber,sectionnumber):
     print(calculatedanchorx,calculatedanchory)
     q12md = getmetadataforblock(sortedanchor[anchornumber],data[sectionnumber])
-    x1 = int(calculatedanchorx + q12md["anchorx2startx"])
-    x2 = int(calculatedanchorx + q12md["anchorx2endx"])
-    y1 = int(calculatedanchory + q12md["anchory2starty"])
-    y2 = int(calculatedanchory + q12md["anchory2endy"])
-    q = {
-        "region": [y1,y2,x1,x2], # for resized image of 800,1200
-    }
-    for option in q12md["anchor2options"]:
-        x1 = int(calculatedanchorx + option["anchorx2optionstartx"])
-        x2 = int(calculatedanchorx + option["anchorx2optionendx"])
-        y1 = int(calculatedanchory + option["anchory2optionstarty"])
-        y2 = int(calculatedanchory + option["anchory2optionendy"])
-        q[option["optionname"]] = [y1,y2,x1,x2]
+    q = getactualcoordinates(calculatedanchorx,calculatedanchory,q12md)
     region,options = get_image_sections(image,q)
     selected_result = get_result(region,options,True)
     print(q12md["name"],selected_result,q["region"])
@@ -108,6 +141,23 @@ def getsortedanchor(data):
       allanchors.append(elements)
   sortedanchor = sorted(allanchors, key=lambda d: d["start"]["x"])
   return sortedanchor
+
+def getactualcoordinates(calculatedanchorx,calculatedanchory,q12md):
+  x1 = int(calculatedanchorx + q12md["anchorx2startx"])
+  x2 = int(calculatedanchorx + q12md["anchorx2endx"])
+  y1 = int(calculatedanchory + q12md["anchory2starty"])
+  y2 = int(calculatedanchory + q12md["anchory2endy"])
+  q = {
+      "region": [y1,y2,x1,x2], # for resized image of 800,1200
+  }
+  for option in q12md["anchor2options"]:
+    x1 = int(calculatedanchorx + option["anchorx2optionstartx"])
+    x2 = int(calculatedanchorx + option["anchorx2optionendx"])
+    y1 = int(calculatedanchory + option["anchory2optionstarty"])
+    y2 = int(calculatedanchory + option["anchory2optionendy"])
+    q[option["optionname"]] = [y1,y2,x1,x2]
+  return q
+
 
 def processoneimage(payloaddata,template,image,anchornumber):
     sortedanchor = getsortedanchor(payloaddata)
@@ -131,27 +181,22 @@ def processoneimage(payloaddata,template,image,anchornumber):
     # plt.imshow(image)
     # plt.plot(peaks_bottom[:,1], peaks_bottom[:,0], 'o', markeredgecolor='r', markerfacecolor='none', markersize=2)
     # plt.plot(peaks_top[:,1], peaks_top[:,0], 'o', markeredgecolor='r', markerfacecolor='none', markersize=2)
-    print(calculatedanchorx,calculatedanchory)
-    showsectionandgetregion(image,data,calculatedanchorx,calculatedanchory,sortedanchor,anchornumber,35)
+    # print(calculatedanchorx,calculatedanchory)
+    # showsectionandgetregion(image,data,calculatedanchorx,calculatedanchory,sortedanchor,anchornumber,35)
 
     datadict = {}
-    datadict["imagename"] = "test"
+    rollnumber = ""
+    for i in range(6,16):
+      q12md = getmetadataforblock(sortedanchor[anchornumber],data[i])
+      q = getactualcoordinates(calculatedanchorx,calculatedanchory,q12md)
+      region,options = get_image_sectionsv2(image,q)
+      selected_result = get_roll(region,options)
+      rollnumber += selected_result
+    datadict["imagename"] = rollnumber
     for i in range(16,len(data)):
         q12md = getmetadataforblock(sortedanchor[anchornumber],data[i])
-        x1 = int(calculatedanchorx + q12md["anchorx2startx"])
-        x2 = int(calculatedanchorx + q12md["anchorx2endx"])
-        y1 = int(calculatedanchory + q12md["anchory2starty"])
-        y2 = int(calculatedanchory + q12md["anchory2endy"])
-        q = {
-            "region": [y1,y2,x1,x2], # for resized image of 800,1200
-        }
-        for option in q12md["anchor2options"]:
-            x1 = int(calculatedanchorx + option["anchorx2optionstartx"])
-            x2 = int(calculatedanchorx + option["anchorx2optionendx"])
-            y1 = int(calculatedanchory + option["anchory2optionstarty"])
-            y2 = int(calculatedanchory + option["anchory2optionendy"])
-            q[option["optionname"]] = [y1,y2,x1,x2]
-        region,options = get_image_sections(image,q)
+        q = getactualcoordinates(calculatedanchorx,calculatedanchory,q12md)
+        region,options = get_image_sectionsv2(image,q)
         selected_result = get_result(region,options)
         datadict[q12md['name']] = selected_result
         # print(f"{q12md['name']}-{selected_result}")
@@ -160,12 +205,16 @@ def processoneimage(payloaddata,template,image,anchornumber):
     
 
 if __name__ == "__main__":
+    anchornumber = 2
     data = readjson('payload.json')
+    createmetadatfile(anchornumber,data)
+    metadata = None
+    with open('metadata.json', 'r') as f:
+      metadata = json.load(f)
     template = io.imread("4.jpg")
     df_concat = pd.DataFrame()
     for images in os.listdir("./imgdata"):
         image = io.imread(os.path.join("imgdata",images))
-        anchornumber = 0
         print(images)
         df = processoneimage(data,template,image,anchornumber)
         df_concat = pd.concat([df_concat, df], axis=0)
